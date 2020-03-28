0e8d30112fd38fb3db5a566ab8b722f5bab929fa
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-03-28 18:47:46 +0530

    [BHUSHAN]Refactor:Counting the Dia how many times throws.

--- SnakeLadderGame.sh
+++ SnakeLadderGame.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 echo "==> Welcome to Snake and Ladder Game <<=="
-     initialPosition=0
-     currentPosition=0
+    initialPosition=0
+    currentPosition=0
      NoPlay=0
      Ladder=1
      Snake=2
      winnigPosition=100
+     counter=0
 
     function randomValue(){
+        ((counter++))
         getValue=$((RANDOM%6+1))
         echo "Dice the Number:" $getValue
     }
+
     randomValue
 
-    function Check_NoPlay_Ladder_Snake(){
+      function Check_NoPlay_Ladder_Snake()
+	{
 
         position=$((RANDOM%3))
-        case $position in
+
+	  case $position in
         $NoPlay)
+
              echo "For No Play=" $position
-            currentPosition=$currentPosition
+            currentPosition=$currentPosition    
         ;;
 
         $Ladder)
@@ -28,26 +34,27 @@ echo "==> Welcome to Snake and Ladder Game <<=="
         ;;
 
         $Snake)
-        	echo "For Snake=" $position
-	        currentPosition=$(($currentPosition - $getValue))
-            	    if [ $currentPosition -lt 0 ]
-            	    then
-                	currentPosition=0
-           	    fi
+            echo "For Snake=" $position
+            currentPosition=$(($currentPosition - $getValue))
+
+         if [ $currentPosition -lt 0 ]
+         then
+                currentPosition=0
+         fi
         ;;
         esac
-             echo "Current Position=" $currentPosition
+        echo "Current Position=" $currentPosition
     }
     Check_NoPlay_Ladder_Snake
 
 	while [ $currentPosition -lt $winnigPosition ]
-        do
-         randomValue
-         Check_NoPlay_Ladder_Snake
-
-	if [ $currentPosition -gt $winnigPosition ]
-        then
-            currentPosition=$(( $currentPosition - $getValue ))
-            echo " Current Position="$currentPosition
-        fi
-    done
+    	do
+        	randomValue
+        	Check_NoPlay_Ladder_Snake
+            if [ $currentPosition -gt $winnigPosition ]
+            then
+            	currentPosition=$(( $currentPosition - $getValue ))
+            	echo " Current Position="$currentPosition
+            fi
+        done
+  echo "Number of Time dice played" $counter
