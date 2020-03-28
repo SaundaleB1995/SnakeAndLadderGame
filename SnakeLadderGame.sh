7924d2cc69e825d4bb84905ce4e6b821d05f3d49
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-03-28 18:25:30 +0530

    [BHUSHAN]Refactor:Repeat till Player Reach win Position code.

--- SnakeLadderGame.sh
+++ SnakeLadderGame.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 echo "==> Welcome to Snake and Ladder Game <<=="
-    
+    initialPosition=0
     currentPosition=0
      NoPlay=0
      Ladder=1
      Snake=2
+     winnigPosition=100
+
 
     function randomValue(){
+
         getValue=$((RANDOM%6+1))
         echo "Dice the Number:" $getValue
     }
@@ -17,26 +20,33 @@ echo "==> Welcome to Snake and Ladder Game <<=="
 	        position=$((RANDOM%3))
 
 	case $position in
-        	$NoPlay)
-             		echo "For No Play=" $position
-            		currentPosition=$currentPosition    
-        	;;
+        $NoPlay)
+	         echo "For No Play=" $position
+        	 currentPosition=$currentPosition
+        ;;
 
-        	$Ladder)
-		        echo "For Ladder=" $position
-            		currentPosition=$(($currentPosition + $getValue))
-        	;;
+        $Ladder)
+            	echo "For Ladder=" $position
+            	currentPosition=$(($currentPosition + $getValue))
+        ;;
 
-       	 	$Snake)
-		        echo "For Snake=" $position
-            		currentPosition=$(($currentPosition - $getValue))
+        $Snake)
+            	echo "For Snake=" $position
+            	currentPosition=$(($currentPosition - $getValue))
 
-		     if [ $currentPosition -lt 0 ]
-        	     then
+		if [ $currentPosition -lt 0 ]
+        	then
                 	currentPosition=0
-        	      fi
-        	;;
-        	esac
+        	fi
+        ;;
+        esac
         	echo "Current Position=" $currentPosition
-    	}
-	    Check_NoPlay_Ladder_Snake
+    }
+    Check_NoPlay_Ladder_Snake
+
+    while [ $currentPosition -lt $winnigPosition ]
+    do
+	        randomValue
+        	Check_NoPlay_Ladder_Snake
+    
+    done
