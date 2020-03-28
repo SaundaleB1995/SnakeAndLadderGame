c36f61ce10cd3d22ae5414cfb7601de9c5a7bd80
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-03-28 17:58:23 +0530

    [BHUSHAN]Refactor:Player roll dia number between 1 to 6.

--- SnakeLadderGame.sh
+++ SnakeLadderGame.sh
@@ -1,9 +1,11 @@
-
 #!/bin/bash
 echo "==> Welcome to Snake and Ladder Game <<=="
 
     initialPosition=0
     currentPosition=$initialPosition
 
-    echo "Initial Position:" $initialPosition
-    echo "Player Position:" $currentPosition
+    function randomValue(){
+        getValue=$((RANDOM%6+1))
+        echo "Dice the Number:" $getValue
+    }
+    randomValue
